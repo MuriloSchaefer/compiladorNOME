f9c3997b0f2c1598923ac718673495e6f03a2902
MuriloSchaefer <murilo.schaefer@gmail.com>
2017-09-07 10:23:38 -0300

    The beginning of everything

--- src/compiladornome/AnalisadorLexico.java
+++ src/compiladornome/AnalisadorLexico.java
@@ -16,14 +16,39 @@ import java.io.IOException;
  */
 public class AnalisadorLexico {
     private File file = null;
+    private FileInputStream fis = null;
     
     public AnalisadorLexico(File file) throws FileNotFoundException, IOException{
-        this.file = file;
+        this.file = file;        
+        this.fis = new FileInputStream(file);
+    }
+    
+    public Token reconhecePalavra() throws IOException{
         char current;
-        FileInputStream fis = new FileInputStream(file);
-        while (fis.available() > 0) {
-          current = (char) fis.read();
-          System.out.println(current);
+        Token res = null;
+        while (this.fis.available() > 0) {
+          current = (char) this.fis.read();
+          switch(current){
+              case '#': res = reconheceId(); break; 
+          }
         }
+        return res;
+    }
+    
+    public Token reconheceId() throws IOException{
+        Token res = new Token("Id", null);
+        String valor = "#";
+        char current;
+        int fim = 0;
+        while (this.fis.available() > 0 && fim != 1) {
+          current = (char) this.fis.read();
+          switch(current){
+              case ' ': fim = 1; break;
+              default: valor += current; break;
+          }
+        }
+        //voltar um caracter
+        res.setValor(valor);
+        return res;
     }
 }
---
+++ src/compiladornome/Token.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compiladornome;
+
+/**
+ *
+ * @author murilo
+ */
+public class Token {
+    private String atributo;
+    private String valor;
+
+    public Token(String atributo, String valor) {
+        this.atributo = atributo;
+        this.valor = valor;
+    }
+
+    public String getAtributo() {
+        return atributo;
+    }
+
+    public void setAtributo(String atributo) {
+        this.atributo = atributo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+    
+    
+}
